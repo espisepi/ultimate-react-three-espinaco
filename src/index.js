4e603352cc6190412e55618f9f89db8cab78f23c
espisepi <josdomesp@gmail.com>
2023-01-06 23:25:01 +0100

    change div id root name

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("ultimate-react-three-espinaco"));
 root.render(
   <React.StrictMode>
     <App />
