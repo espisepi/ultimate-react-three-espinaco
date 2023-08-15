0e5535a6b230fbe8cbeb84cdda3ee56e06c0e399
espisepi <sepinaco@icloud.com>
2023-08-15 17:51:29 +0200

    reorganize project and improve ui background

---
+++ src/espinaco/apps/app1/App1.js
@@ -0,0 +1,218 @@
+import React, { useEffect, useState, useRef, useCallback } from "react";
+
+// import SceneManager from "./espinaco/scenes/manager/SceneManager";
+
+// import { NippleJoystick } from "./espinaco/controls/NippleJoystick";
+
+import { FullScreen, useFullScreenHandle } from "react-full-screen";
+import Scene1Canvas from "../../scenes/Scene1";
+
+const BASE_URL_HEROKU_VIDEO_YT_DL =
+  "https://video-dl-esp.herokuapp.com/video/video?url=";
+const BASE_URL_LOCAL_VIDEO_YT_DL = "http://localhost:4000/video/video?url=";
+
+// let window_url_youtube = window.urlYoutube || "";
+// window.urlYoutube = window_url_youtube;
+
+// let window_show_video = window.showVideo || true;
+// window.showVideo = window_show_video;
+
+// const window_urlYoutube = 'videos/mcpi.mp4';
+// const window_urlYoutube = window.urlYoutube || BASE_URL_HEROKU_VIDEO_YT_DL + "https://www.youtube.com/watch?v=MaaEVFNDQLo";
+const window_showVideo = window.showVideo || true;
+// const INIT_STATE = { window_urlYoutube, window_showVideo };
+
+// (COMENTARIO ANTIGUO DE CUANDO REALIZABA LA BUILD A MANO Y LA INCLUIA EN WORDPRESS COPIANDO Y PEGANDO LA CARPETA BUILD POR FTP, AHORA LO HACEMOS CON MICROFRONTEND DESPLEGADO EN UNA URL JEJE) TODO ACORDARSE!!!: CADA VEZ QUE SE HACE BUILD HAY QUE CAMBIAR LOS NOMBRES DE LOS FICHEROS JS Y CSS GENERADOS EN LA PAGINA DE WORDPRESS.
+
+const dataMusic = [
+  {
+    name: "Tove Lo - Habits (Stay High)",
+    link: "videos/stayHigh.mp4",
+  },
+  {
+    name: "HOKE - MOONDIAL",
+    link: "videos/HOKE-MOONDIAL.mp4",
+  },
+  {
+    name: "youtube",
+    link:
+      BASE_URL_HEROKU_VIDEO_YT_DL +
+      "https://www.youtube.com/watch?v=ZelTFpXStE8",
+    // link:'http://localhost:4000/video/video?url=https://www.youtube.com/watch?v=0wa1HzC7OY8'  // For Testing in local
+  },
+  {
+    name: "Kaydy Cain - Perdedores del Barrio, highkili ay linda,",
+    link:
+      BASE_URL_HEROKU_VIDEO_YT_DL +
+      "https://www.youtube.com/watch?v=ZelTFpXStE8",
+  },
+  {
+    name: "sotoasa jugador, trueno, ",
+    link:
+      BASE_URL_HEROKU_VIDEO_YT_DL +
+      "https://www.youtube.com/watch?v=ZelTFpXStE8",
+  },
+  {
+    name: "Kaydy Cain - Perdedores del Barrio",
+    link:
+      BASE_URL_HEROKU_VIDEO_YT_DL +
+      "https://www.youtube.com/watch?v=ZelTFpXStE8",
+  },
+];
+
+export default function App1({ url }) {
+  const [clicked, setClicked] = useState(false);
+
+  const [showVideo, setShowVideo] = useState(window_showVideo);
+  const handleShowVideo = useCallback(() => {
+    setShowVideo((v) => !showVideo);
+  }, [showVideo]);
+
+  // let firstUrl = url ? BASE_URL_HEROKU_VIDEO_YT_DL + url : window_urlYoutube;
+  const firstUrl = "videos/stayHigh.mp4";
+  const [link, setLink] = useState(firstUrl);
+
+  const handleInputText = useCallback((event) => {
+    const youtubeUrl = event.target.value;
+    setLink((v) => BASE_URL_HEROKU_VIDEO_YT_DL + youtubeUrl);
+  }, []);
+
+  const handleFullScreen = useFullScreenHandle();
+  const toggleFullScreen = useCallback(() => {
+    if (handleFullScreen.active) {
+      handleFullScreen.exit();
+    } else {
+      handleFullScreen.enter();
+    }
+  }, [handleFullScreen]);
+
+  const handleAutoRotate = useCallback(() => {
+    if (window.orbitControls) {
+      window.orbitControls.autoRotate = !window.orbitControls.autoRotate;
+    }
+  }, []);
+
+  // TODO: UI Para mostrar todas las canciones y poder cambiar de cancion en la lista de reproduccion que he hecho (la variable dataMusic)
+
+  if (clicked) {
+    return (
+      <div id="app-espinaco" style={{ position: "relative" }}>
+        <FullScreen handle={handleFullScreen}>
+          <Scene1Canvas
+            style={{
+              position: "relative",
+              top: "0",
+              width: "100%",
+              height: "100vh",
+            }}
+          />
+
+          <video
+            id="video"
+            style={{
+              display: showVideo ? "block" : "none",
+              width: "25vw",
+              height: "25vh",
+              top: 0,
+              zIndex: 100,
+              position: "absolute",
+            }}
+            src={link}
+            controls={true}
+            autoPlay={true}
+            crossOrigin="anonymous"
+          ></video>
+
+          {/* <video id="video" style={{ display: showVideo ? 'block' : 'none', width: '25vw', height: '25vh', top: 0, zIndex: 100, position: 'absolute' }}
+            src={link} controls={true} autoPlay={true} crossOrigin="anonymous"></video> */}
+
+          {/* <div id="ui-controls-godCamera" style={{ display: showVideo ? 'block' : 'none', position: "relative" }}> */}
+          {/* Aqui se ponen botones visuales para manejar la camara para todos los lados -> Asociar cada boton visual a un boton de teclado cuando se pulse */}
+          {/* <NippleJoystick style={{ display: showVideo ? 'block' : 'none' }} /> */}
+          {/* </div> */}
+
+          <input
+            type="text"
+            placeholder="Insert url from youtube like https://www.youtube.com/watch?v=ZelTFpXStE8"
+            onChange={handleInputText}
+            style={{
+              display: showVideo ? "block" : "none",
+              border: "none",
+              borderRadius: "4px",
+              width: "50vw",
+              height: "30px",
+              position: "absolute",
+              top: "20px",
+              left: "40%",
+            }}
+          />
+
+          <button
+            onClick={handleAutoRotate}
+            style={{
+              display: showVideo ? "block" : "none",
+              width: "50px",
+              height: "50px",
+              borderRadius: "25px",
+              position: "absolute",
+              bottom: "100px",
+              right: "200px",
+              backgroundColor: "#ffff00",
+              opacity: 0.5,
+            }}
+          >
+            {" "}
+          </button>
+
+          <button
+            onClick={toggleFullScreen}
+            style={{
+              display: showVideo ? "block" : "none",
+              width: "50px",
+              height: "50px",
+              borderRadius: "25px",
+              position: "absolute",
+              bottom: "100px",
+              right: "100px",
+              backgroundColor: "#ff00ff",
+              opacity: 0.5,
+            }}
+          >
+            {" "}
+          </button>
+
+          <button
+            onClick={handleShowVideo}
+            style={{
+              width: "50px",
+              height: "50px",
+              borderRadius: "25px",
+              position: "absolute",
+              bottom: "100px",
+              right: "10px",
+              backgroundColor: "white",
+              opacity: showVideo ? 1 : 0.3,
+            }}
+          ></button>
+        </FullScreen>
+      </div>
+    );
+  }
+
+  return (
+    <div
+      className="background-initial"
+      style={{
+        display: "flex",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+        color: "black",
+        backgroundColor: "#500050",
+      }}
+      onClick={() => setClicked(true)}
+    >
+      <h1>Click to Start</h1>
+    </div>
+  );
+}
--- src/espinaco/scenes/Scene1.js
+++ src/espinaco/scenes/Scene1.js
@@ -1,22 +1,19 @@
+import React, { useCallback } from "react";
 
-import React, { useCallback } from 'react';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, Box, Stars } from "@react-three/drei";
 
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Box, Stars } from '@react-three/drei';
+import { BoxVideo, BoxShader } from "../prefabs/BoxCustom";
+import VideoPoints from "../prefabs/VideoPoints";
+import { Suspense } from "react";
 
-import { BoxVideo, BoxShader } from '../prefabs/BoxCustom';
-import VideoPoints from '../prefabs/VideoPoints';
-import { Suspense } from 'react';
-
-import GodCameraControls from '../controls/GodCameraControls';
-import { MusicVisualCubeReact } from '../prefabs/musicVisualCube/MusicVisualCube';
-import { SubtitleMesh } from '../prefabs/subtitleMesh/SubtitleMesh';
+import GodCameraControls from "../controls/GodCameraControls";
+import { MusicVisualCubeReact } from "../prefabs/musicVisualCube/MusicVisualCube";
+import { SubtitleMesh } from "../prefabs/subtitleMesh/SubtitleMesh";
 
 // import { MeshSurfaceSampler } from '../prefabs/meshSurfaceSampler/MeshSurfaceSampler';
 
-import { MemoryCardGame } from '../games/memoryCardGame/MemoryCardGame';
-
-
+import { MemoryCardGame } from "../games/memoryCardGame/MemoryCardGame";
 
 // Coger efectos de codrops
 // Mezclar esos dos efectos para soto asa videoclip
@@ -25,15 +22,17 @@ import { MemoryCardGame } from '../games/memoryCardGame/MemoryCardGame';
 // https://tympanus.net/Tutorials/SurfaceSampling/index3.html
 // Para efectos de letras: https://tympanus.net/Development/3DTypeEffects/03_flowers.html
 
-
 export function Scene1() {
-    return (
-        <>
-        <ambientLight />
-        {/* <Box /> */}
-        {/* <BoxVideo /> */}
-        {/* <BoxShader /> */}
-        <group>
+  return (
+    <>
+      <ambientLight />
+      {/* <Box /> */}
+      {/* <BoxVideo /> */}
+      {/* <BoxShader /> */}
+
+      <VideoPoints />
+
+      {/* <group>
             <group>
                 <VideoPoints />
                 <VideoPoints position={[30,0,0]}/>
@@ -42,8 +41,9 @@ export function Scene1() {
                 <VideoPoints />
                 <VideoPoints position={[30,0,0]}/>
             </group>
-        </group>
-        {/* <group position={[100,100,0]}>
+        </group> */}
+
+      {/* <group position={[100,100,0]}>
             <group>
                 <VideoPoints />
                 <VideoPoints position={[30,0,0]}/>
@@ -53,31 +53,40 @@ export function Scene1() {
                 <VideoPoints position={[30,0,0]}/>
             </group>
         </group> */}
-        {/* <MusicVisualCubeReact /> */}
-        {/* <SubtitleMesh /> */}
-        {/* <MeshSurfaceSampler /> */}
-        <Stars radius={1000}  count={9999} depth={400} factor={30} fade/* saturation={1} */ /* speed={1} */ />
-        </>
-    )
+
+      {/* <MusicVisualCubeReact /> */}
+      {/* <SubtitleMesh /> */}
+      {/* <MeshSurfaceSampler /> */}
+      <Stars
+        radius={1000}
+        count={9999}
+        depth={400}
+        factor={30}
+        fade /* saturation={1} */ /* speed={1} */
+      />
+    </>
+  );
 }
 
-
-
 export default function Scene1Canvas({ style }) {
-    return (
-        <>
-        <Canvas style={{...style, backgroundColor:'black'}} camera={{position:[0,0,400], far: 99999 }}>
-            <Suspense fallback={<Box material-color='red' material-wireframe='true'/>}>
-                <Scene1/>
-            </Suspense>
+  return (
+    <>
+      <Canvas
+        style={{ ...style, backgroundColor: "black" }}
+        camera={{ position: [0, 0, 400], far: 99999 }}
+      >
+        <Suspense
+          fallback={<Box material-color="red" material-wireframe="true" />}
+        >
+          <Scene1 />
+        </Suspense>
 
-            {/* <Suspense fallback={<Box material-color='green' material-wireframe='true'/>}>
+        {/* <Suspense fallback={<Box material-color='green' material-wireframe='true'/>}>
                 <MemoryCardGame />
             </Suspense> */}
 
-
-            <GodCameraControls /> 
-        </Canvas>
-        </>
-    )
+        <GodCameraControls />
+      </Canvas>
+    </>
+  );
 }
