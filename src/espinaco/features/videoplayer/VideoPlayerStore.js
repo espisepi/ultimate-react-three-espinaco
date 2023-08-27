fecf793cbc7503e7ab38344c0902459dd5cbb9bd
espisepi <sepinaco@icloud.com>
2023-08-27 12:33:47 +0200

    added song

--- src/espinaco/features/videoplayer/VideoPlayerStore.js
+++ src/espinaco/features/videoplayer/VideoPlayerStore.js
@@ -58,6 +58,10 @@ export const useVideoPlayerStore = create((set, get) => ({
       name: "Alemán - Maradona (Visualizer)",
       url: "https://www.youtube.com/watch?v=4DFxeyPiRkM",
     },
+    {
+      name: "D.NADIE - LOS OJOS COMO LUNAS (Visualizer)",
+      url: "https://www.youtube.com/watch?v=n8OKNX4NpSA",
+    },
   ].sort((a, b) => a.name.localeCompare(b.name)), // Reference1 (definida al principio de este fichero)
   selectedVideo: null,
   //selectVideo: (video) => set({ selectedVideo: video }),
