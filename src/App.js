715303c8da1319de61ebe0517067330ef1d8bdf6
espisepi <sepinaco@icloud.com>
2023-08-31 11:45:47 +0200

    added vercel analytics

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import "./App.css";
 
 import App1 from "./espinaco/apps/app1/App1";
 
+import { inject } from "@vercel/analytics";
+
 export default function App() {
+  useEffect(() => {
+    inject();
+  }, []);
   return <App1 />;
 }
