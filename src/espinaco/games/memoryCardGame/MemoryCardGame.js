6e742b29ffd95f1e481c2b19f999c7eada4a982b
espisepi <sepinaco@icloud.com>
2023-12-28 18:28:11 +0100

    updated three package.json

--- src/espinaco/games/memoryCardGame/MemoryCardGame.js
+++ src/espinaco/games/memoryCardGame/MemoryCardGame.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import { useEffect, useRef, useMemo, useState } from 'react';
-import { Box, useTexture } from '@react-three/drei';
+import React from "react";
+import { useEffect, useRef, useMemo, useState } from "react";
+import { Box, useTexture } from "@react-three/drei";
 
-import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js'
-import { useFrame } from '@react-three/fiber';
+import * as TWEEN from "three/examples/jsm/libs/tween.module.js";
+import { useFrame } from "@react-three/fiber";
 
-import { GameManager } from './GameManager';
+import { GameManager } from "./GameManager";
 
 /* El codigo esta dentro del <Canvas> de r3f, asi que podemos ejecutar los hooks de r3f like useThree and useFrame
     Tutorial usar TWEEN con threejs
@@ -13,172 +13,178 @@ import { GameManager } from './GameManager';
     https://sbcode.net/view_source/tween.html
 */
 
-
-const BASE_IMG_SRC = 'images/'
+const BASE_IMG_SRC = "images/";
 
 const dataGame = {
-    defaultImage: BASE_IMG_SRC + 'logo192.png',
-    images: [
-        BASE_IMG_SRC + 'cardFrontTest.png',
-        BASE_IMG_SRC + 'cardFrontTest.png'
-    ]
-}
+  defaultImage: BASE_IMG_SRC + "logo192.png",
+  images: [
+    BASE_IMG_SRC + "cardFrontTest.png",
+    BASE_IMG_SRC + "cardFrontTest.png",
+  ],
+};
 
-export function MemoryCardGame({numberRows = 3, numberCols = 3}) {
+export function MemoryCardGame({ numberRows = 3, numberCols = 3 }) {
+  // const texture = useTexture([dataGame.defaultImage])
+  const [defaultTexture, ...textures] = useTexture([
+    dataGame.defaultImage,
+    ...dataGame.images,
+  ]);
 
-    // const texture = useTexture([dataGame.defaultImage])
-    const [defaultTexture, ...textures] = useTexture([dataGame.defaultImage, ...dataGame.images]);
-
-    const positionsCards = useMemo(()=>{
-        const res = [];
-            for(let i = 0; i < numberRows; i++) {
-                for(let j = 0; j < numberCols; j++) {
-                    const position = [i,j,0];
-                    res.push(position);
-                }
-            }
-        return res;
-    },[numberRows, numberCols])
-
-
-    const cardsRef = useRef();
-    const [gameManager, setGameManager] = useState(null);
-    useEffect(()=>{
-        if(cardsRef.current && gameManager === null) {
-            setGameManager((v)=>new GameManager(cardsRef.current));
-        }
-    },[cardsRef])
-
-    // useFrame(({clock})=>{
-    //     if(gameManager) {
-    //         gameManager.update();
-    //     }
-    // });
-
-    return (
-        <>
-        <group ref={cardsRef} name='cards' scale={[50,50,50]}>
-            {positionsCards.map((position, i) => (
-                <>
-                    <Card name={`card-${i}-0`} position={[positionsCards[i][0],positionsCards[i][1],positionsCards[i][2]]} defaultTexture={defaultTexture} texture={textures[i % textures.length]} scale={[1,1,1]} />
-                    {/* <Card name={`card-${i}-1`} position={[positionsCards[i+1][0],positionsCards[i+1][1],positionsCards[i+1][2]]} defaultTexture={defaultTexture} texture={textures[i % textures.length]} scale={[1,1,1]} /> */}
-                </>
-            ))}
-        </group>
-        </>
-    );
-    // return <Card defaultTexture={defaultTexture} texture={textures[0]} name="card-1" scale={[50,50,50]} />;    
-}
-
-
-
-
-export function Card({defaultTexture, texture, ...props}) {
-
-    const meshRef = useRef();
-
-    useEffect(()=>{
-        const meshCurrent = meshRef.current;
-        if(meshCurrent && !meshCurrent.userData.isLoaded) {
-            // LOAD userData game to mesh
-            //TODO: meshCurrent.userDat.card = new Card(meshCurrent); Y en class Card pongo todos esos atributos que estoy creando
-            meshCurrent.userData.isLoaded = true;
-            meshCurrent.userData.showCard = false; // Cuando es true mostramos la imagen a memorizar y cuando es false mostramos la imagen por defecto de todas
-            meshCurrent.userData.isClickEnabled = true;
-            meshCurrent.userData.startAnimationFlipCardToTexture = () => {
-                startTweenAnimationFlipCard(meshCurrent,texture);
-            }
-            meshCurrent.userData.startAnimationFlipCardToDefaultTexture = () => {
-                startTweenAnimationFlipCard(meshCurrent,defaultTexture);
-            }
-        }
-    },[meshRef])
-
-    const handleClick = () => {
-        const meshCurrent = meshRef.current;
-        const isClickEnabled = meshCurrent.userData?.isClickEnabled;
-        // FLIP CARD
-        const showCard = !meshCurrent.userData?.showCard;
-        if(meshCurrent && isClickEnabled && showCard ) {
-            // SAVE FLIP CARD
-            meshCurrent.userData.showCard = !meshCurrent.userData.showCard
-            // Mostramos la texture con animacion
-            meshCurrent.userData.startAnimationFlipCardToTexture();
-            // startTweenAnimationFlipCard(meshCurrent, texture);
-            // meshCurrent.material.map = texture;
-            // meshCurrent.material.needsUpdate = true;
-
-        } else {
-            // SAVE FLIP CARD
-            meshCurrent.userData.showCard = !meshCurrent.userData.showCard
-            // Mostramos la default texture con animacion
-            meshCurrent.userData.startAnimationFlipCardToDefaultTexture();
-            // startTweenAnimationFlipCard(meshCurrent, defaultTexture);
-            // meshCurrent.material.map = defaultTexture;
-            // meshCurrent.material.needsUpdate = true;
-
-        }
-
+  const positionsCards = useMemo(() => {
+    const res = [];
+    for (let i = 0; i < numberRows; i++) {
+      for (let j = 0; j < numberCols; j++) {
+        const position = [i, j, 0];
+        res.push(position);
+      }
     }
+    return res;
+  }, [numberRows, numberCols]);
 
-    useFrame(()=>{
-        TWEEN.update();
-    });
+  const cardsRef = useRef();
+  const [gameManager, setGameManager] = useState(null);
+  useEffect(() => {
+    if (cardsRef.current && gameManager === null) {
+      setGameManager((v) => new GameManager(cardsRef.current));
+    }
+  }, [cardsRef]);
 
-    return (
-        <Box ref={meshRef} material-map={defaultTexture} {...props} onClick={handleClick}/>
-    );
+  // useFrame(({clock})=>{
+  //     if(gameManager) {
+  //         gameManager.update();
+  //     }
+  // });
+
+  return (
+    <>
+      <group ref={cardsRef} name="cards" scale={[50, 50, 50]}>
+        {positionsCards.map((position, i) => (
+          <>
+            <Card
+              name={`card-${i}-0`}
+              position={[
+                positionsCards[i][0],
+                positionsCards[i][1],
+                positionsCards[i][2],
+              ]}
+              defaultTexture={defaultTexture}
+              texture={textures[i % textures.length]}
+              scale={[1, 1, 1]}
+            />
+            {/* <Card name={`card-${i}-1`} position={[positionsCards[i+1][0],positionsCards[i+1][1],positionsCards[i+1][2]]} defaultTexture={defaultTexture} texture={textures[i % textures.length]} scale={[1,1,1]} /> */}
+          </>
+        ))}
+      </group>
+    </>
+  );
+  // return <Card defaultTexture={defaultTexture} texture={textures[0]} name="card-1" scale={[50,50,50]} />;
 }
 
+export function Card({ defaultTexture, texture, ...props }) {
+  const meshRef = useRef();
 
+  useEffect(() => {
+    const meshCurrent = meshRef.current;
+    if (meshCurrent && !meshCurrent.userData.isLoaded) {
+      // LOAD userData game to mesh
+      //TODO: meshCurrent.userDat.card = new Card(meshCurrent); Y en class Card pongo todos esos atributos que estoy creando
+      meshCurrent.userData.isLoaded = true;
+      meshCurrent.userData.showCard = false; // Cuando es true mostramos la imagen a memorizar y cuando es false mostramos la imagen por defecto de todas
+      meshCurrent.userData.isClickEnabled = true;
+      meshCurrent.userData.startAnimationFlipCardToTexture = () => {
+        startTweenAnimationFlipCard(meshCurrent, texture);
+      };
+      meshCurrent.userData.startAnimationFlipCardToDefaultTexture = () => {
+        startTweenAnimationFlipCard(meshCurrent, defaultTexture);
+      };
+    }
+  }, [meshRef]);
+
+  const handleClick = () => {
+    const meshCurrent = meshRef.current;
+    const isClickEnabled = meshCurrent.userData?.isClickEnabled;
+    // FLIP CARD
+    const showCard = !meshCurrent.userData?.showCard;
+    if (meshCurrent && isClickEnabled && showCard) {
+      // SAVE FLIP CARD
+      meshCurrent.userData.showCard = !meshCurrent.userData.showCard;
+      // Mostramos la texture con animacion
+      meshCurrent.userData.startAnimationFlipCardToTexture();
+      // startTweenAnimationFlipCard(meshCurrent, texture);
+      // meshCurrent.material.map = texture;
+      // meshCurrent.material.needsUpdate = true;
+    } else {
+      // SAVE FLIP CARD
+      meshCurrent.userData.showCard = !meshCurrent.userData.showCard;
+      // Mostramos la default texture con animacion
+      meshCurrent.userData.startAnimationFlipCardToDefaultTexture();
+      // startTweenAnimationFlipCard(meshCurrent, defaultTexture);
+      // meshCurrent.material.map = defaultTexture;
+      // meshCurrent.material.needsUpdate = true;
+    }
+  };
+
+  useFrame(() => {
+    TWEEN.update();
+  });
+
+  return (
+    <Box
+      ref={meshRef}
+      material-map={defaultTexture}
+      {...props}
+      onClick={handleClick}
+    />
+  );
+}
 
 function startTweenAnimationFlipCard(mesh, texture) {
-    mesh.material.needsUpdate = true;
-    new TWEEN.Tween(mesh.position)
-                        .to(
-                            {
-                                z: 0 + 3,
-                            },
-                            500
-                        )
-                        .easing(TWEEN.Easing.Cubic.Out)
-                        // .onUpdate(() => render())
-                        .start()
-                        .onComplete(() => {
-                            new TWEEN.Tween(mesh.position)
-                                .to(
-                                    {
-                                        z: 0
-                                    },
-                                    500
-                                )
-                                .easing(TWEEN.Easing.Bounce.Out)
-                                // .onUpdate(() => render())
-                                .start()
-                        });
-    new TWEEN.Tween(mesh.rotation)
-                        .to(
-                            {
-                                y: 0 + 10,
-                            },
-                            250
-                        )
-                        .easing(TWEEN.Easing.Cubic.Out)
-                        // .onUpdate(() => render())
-                        .start()
-                        .onComplete(() => {
-                            new TWEEN.Tween(mesh.rotation)
-                                .to(
-                                    {
-                                        y: 0,
-                                    },
-                                    250
-                                )
-                                .easing(TWEEN.Easing.Bounce.Out)
-                                .onUpdate(() => {
-                                    // Change texture
-                                    mesh.material.map = texture;
-                                })
-                                .start()
-                        });
+  mesh.material.needsUpdate = true;
+  new TWEEN.Tween(mesh.position)
+    .to(
+      {
+        z: 0 + 3,
+      },
+      500
+    )
+    .easing(TWEEN.Easing.Cubic.Out)
+    // .onUpdate(() => render())
+    .start()
+    .onComplete(() => {
+      new TWEEN.Tween(mesh.position)
+        .to(
+          {
+            z: 0,
+          },
+          500
+        )
+        .easing(TWEEN.Easing.Bounce.Out)
+        // .onUpdate(() => render())
+        .start();
+    });
+  new TWEEN.Tween(mesh.rotation)
+    .to(
+      {
+        y: 0 + 10,
+      },
+      250
+    )
+    .easing(TWEEN.Easing.Cubic.Out)
+    // .onUpdate(() => render())
+    .start()
+    .onComplete(() => {
+      new TWEEN.Tween(mesh.rotation)
+        .to(
+          {
+            y: 0,
+          },
+          250
+        )
+        .easing(TWEEN.Easing.Bounce.Out)
+        .onUpdate(() => {
+          // Change texture
+          mesh.material.map = texture;
+        })
+        .start();
+    });
 }
--- src/espinaco/prefabs/musicVisualCube/MusicVisualCubeClass.js
+++ src/espinaco/prefabs/musicVisualCube/MusicVisualCubeClass.js
@@ -1,122 +1,144 @@
-
-import * as THREE from 'three';
-
+import * as THREE from "three";
 
 export class MusicVisualCubeClass {
+  constructor(scene, mesh1) {
+    console.log("HOLIIII");
+    this.mesh1 = mesh1;
+    const geometry = this.createGeometry();
+    const material = this.createMaterial();
+    this.mesh = new THREE.Mesh(geometry, material);
+    scene.add(this.mesh);
+  }
 
-    constructor(scene, mesh1) {
-        console.log("HOLIIII");
-        this.mesh1 = mesh1;
-        const geometry = this.createGeometry();
-        const material = this.createMaterial();
-        this.mesh = new THREE.Mesh(geometry, material);
-        scene.add(this.mesh);
+  update() {
+    const time = performance.now();
+    if (this.mesh) {
+      this.mesh.rotation.y = time * 0.0005;
+      this.mesh.material.uniforms["time"].value = time * 0.005;
+      this.mesh.material.uniforms["sineTime"].value = Math.sin(
+        this.mesh.material.uniforms["time"].value * 0.05 + 5.0
+      );
+    }
+  }
+
+  createMaterial() {
+    return new THREE.RawShaderMaterial({
+      uniforms: {
+        time: { value: 1.0 },
+        sineTime: { value: 1.0 },
+      },
+      vertexShader: VERTEXSHADER,
+      fragmentShader: FRAGMENTSHADER,
+      side: THREE.DoubleSide,
+      transparent: true,
+    });
+  }
+
+  createGeometry() {
+    // geometry
+
+    const vector = new THREE.Vector4();
+
+    const instances = 100;
+
+    const positions = [];
+    const offsets = [];
+    const colors = [];
+    const orientationsStart = [];
+    const orientationsEnd = [];
+
+    positions.push(0.025, -0.025, 0);
+    positions.push(-0.025, 0.025, 0);
+    positions.push(0, 0, 0.025);
+
+    // geometry1 y geometry2 para posiciones
+    const boxBufferGeometry = new THREE.BoxGeometry();
+    const boxAttributePosition = boxBufferGeometry.getAttribute("position");
+    const boxAttributePositionArray = boxAttributePosition.array;
+    const sphereBufferGeometry = new THREE.SphereGeometry();
+    const sphereAttributePosition =
+      sphereBufferGeometry.getAttribute("position");
+    const sphereAttributePositionArray = sphereAttributePosition.array;
+
+    // espisepi: AQUI LO DEJO!! TENGO QUE CORREGIR EL PASO DE UNA POSICION A OTRA POR VERTEX
+    console.log(this.mesh1.geometry.getAttribute("position").array);
+
+    const arrayPosition1 = boxAttributePositionArray;
+    const arrayPosition2 = sphereAttributePositionArray;
+
+    // instanced attributes
+
+    for (let i = 0; i < instances; i++) {
+      // offsets
+
+      offsets.push(
+        Math.random() - 0.5,
+        Math.random() - 0.5,
+        Math.random() - 0.5
+      );
+
+      // colors
+
+      colors.push(Math.random(), Math.random(), Math.random(), Math.random());
+
+      // orientation start
+
+      // espisepi: Se hace i%instances para que cuando i sea mayor que instances vuelva a 0 y no de error el array
+      const lengthArrayPosition1 = arrayPosition1.length;
+      // vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+      vector.set(
+        arrayPosition1[i % lengthArrayPosition1],
+        arrayPosition1[(i + 1) % lengthArrayPosition1],
+        arrayPosition1[(i + 2) % lengthArrayPosition1],
+        1.0
+      );
+      vector.normalize();
+
+      orientationsStart.push(vector.x, vector.y, vector.z, vector.w);
+      positions.push(vector.x, vector.y, vector.z);
+
+      // orientation end
+
+      // vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
+      vector.set(
+        arrayPosition2[i % instances],
+        arrayPosition2[(i + 1) % instances],
+        arrayPosition2[(i + 2) % instances],
+        1.0
+      );
+      vector.normalize();
+
+      orientationsEnd.push(vector.x, vector.y, vector.z, vector.w);
     }
 
-    update() {
-        const time = performance.now()
-        if (this.mesh) {
-            this.mesh.rotation.y = time * 0.0005;
-            this.mesh.material.uniforms['time'].value = time * 0.005;
-            this.mesh.material.uniforms['sineTime'].value = Math.sin(this.mesh.material.uniforms['time'].value * 0.05 + 5.0);
-        }
-    }
+    console.log(orientationsStart);
 
-    createMaterial() {
-        return (
-            new THREE.RawShaderMaterial({
-                uniforms: {
-                    'time': { value: 1.0 },
-                    'sineTime': { value: 1.0 }
-                },
-                vertexShader: VERTEXSHADER,
-                fragmentShader: FRAGMENTSHADER,
-                side: THREE.DoubleSide,
-                transparent: true
-            })
-        )
-    }
+    const geometry = new THREE.InstancedBufferGeometry();
+    geometry.instanceCount = instances; // set so its initalized for dat.GUI, will be set in first draw otherwise
 
-    createGeometry() {
-        // geometry
+    geometry.setAttribute(
+      "position",
+      new THREE.Float32BufferAttribute(positions, 3)
+    );
 
-        const vector = new THREE.Vector4();
-
-        const instances = 100;
-
-        const positions = [];
-        const offsets = [];
-        const colors = [];
-        const orientationsStart = [];
-        const orientationsEnd = [];
-
-        positions.push(0.025, - 0.025, 0);
-        positions.push(- 0.025, 0.025, 0);
-        positions.push(0, 0, 0.025);
-
-        // geometry1 y geometry2 para posiciones
-        const boxBufferGeometry = new THREE.BoxBufferGeometry();
-        const boxAttributePosition = boxBufferGeometry.getAttribute("position");
-        const boxAttributePositionArray = boxAttributePosition.array;
-        const sphereBufferGeometry = new THREE.SphereBufferGeometry();
-        const sphereAttributePosition = sphereBufferGeometry.getAttribute("position");
-        const sphereAttributePositionArray = sphereAttributePosition.array;
-
-        // espisepi: AQUI LO DEJO!! TENGO QUE CORREGIR EL PASO DE UNA POSICION A OTRA POR VERTEX
-        console.log(this.mesh1.geometry.getAttribute("position").array)
-
-        const arrayPosition1 = boxAttributePositionArray;
-        const arrayPosition2 = sphereAttributePositionArray;
-
-
-        // instanced attributes
-
-        for (let i = 0; i < instances; i++) {
-
-            // offsets
-
-            offsets.push(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
-
-            // colors
-
-            colors.push(Math.random(), Math.random(), Math.random(), Math.random());
-
-            // orientation start
-
-            // espisepi: Se hace i%instances para que cuando i sea mayor que instances vuelva a 0 y no de error el array
-            const lengthArrayPosition1 = arrayPosition1.length;
-            // vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
-            vector.set(arrayPosition1[i % lengthArrayPosition1], arrayPosition1[(i + 1) % lengthArrayPosition1], arrayPosition1[(i + 2) % lengthArrayPosition1], 1.0);
-            vector.normalize();
-
-            orientationsStart.push(vector.x, vector.y, vector.z, vector.w);
-            positions.push(vector.x, vector.y, vector.z)
-
-
-            // orientation end
-
-            // vector.set( Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1, Math.random() * 2 - 1 );
-            vector.set(arrayPosition2[i % instances], arrayPosition2[(i + 1) % instances], arrayPosition2[(i + 2) % instances], 1.0);
-            vector.normalize();
-
-            orientationsEnd.push(vector.x, vector.y, vector.z, vector.w);
-
-        }
-
-        console.log(orientationsStart)
-
-        const geometry = new THREE.InstancedBufferGeometry();
-        geometry.instanceCount = instances; // set so its initalized for dat.GUI, will be set in first draw otherwise
-
-        geometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
-
-        geometry.setAttribute('offset', new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3));
-        geometry.setAttribute('color', new THREE.InstancedBufferAttribute(new Float32Array(colors), 4));
-        geometry.setAttribute('orientationStart', new THREE.InstancedBufferAttribute(new Float32Array(orientationsStart), 4));
-        geometry.setAttribute('orientationEnd', new THREE.InstancedBufferAttribute(new Float32Array(orientationsEnd), 4));
-        return geometry;
-    }
-
+    geometry.setAttribute(
+      "offset",
+      new THREE.InstancedBufferAttribute(new Float32Array(offsets), 3)
+    );
+    geometry.setAttribute(
+      "color",
+      new THREE.InstancedBufferAttribute(new Float32Array(colors), 4)
+    );
+    geometry.setAttribute(
+      "orientationStart",
+      new THREE.InstancedBufferAttribute(new Float32Array(orientationsStart), 4)
+    );
+    geometry.setAttribute(
+      "orientationEnd",
+      new THREE.InstancedBufferAttribute(new Float32Array(orientationsEnd), 4)
+    );
+    return geometry;
+  }
 }
 
 const VERTEXSHADER = `
