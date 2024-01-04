96114a3800abfb32f6026c3ba340116ada3d0aaa
espisepi <sepinaco@icloud.com>
2024-01-04 19:47:00 +0100

    added button to restore position (still in hello world mode mabroda)

--- src/espinaco/features/gba-js-org/components/IframeGBA.js
+++ src/espinaco/features/gba-js-org/components/IframeGBA.js
@@ -1,20 +1,37 @@
 import * as THREE from "three";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { Float, Html } from "@react-three/drei";
+import { Box, Float, Html } from "@react-three/drei";
 
 export default function IframeGBA() {
   return (
     <>
-      <Html occlude="blending" transform distanceFactor={100}>
-        <iframe
-          title="embed"
-          width={1366}
-          height={1024}
-          src="https://gba.js.org/"
-          frameBorder={0}
-        />
-      </Html>
+      <group position={[0, 0, 0]}>
+        <Html occlude="blending" transform distanceFactor={100}>
+          <button
+            onPointerDown={(ev) => alert("holi")}
+            style={{
+              cursor: "pointer",
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              opacity: 0.2,
+              backgroundColor: "#3d1766",
+              height: "5rem",
+              width: "5rem",
+              borderRadius: "25px",
+            }}
+          ></button>
+          <iframe
+            title="embed"
+            width={1366}
+            height={1024}
+            src="https://gba.js.org/"
+            frameBorder={0}
+          />
+        </Html>
+      </group>
     </>
   );
 }
