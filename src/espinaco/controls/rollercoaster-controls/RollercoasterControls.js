0492c8cc7af83cbd73b64b4cd86948bf679c1ce1
espisepi <sepinaco@gmail.com>
2024-06-21 21:19:09 +0200

    change useffect by usememo

--- src/espinaco/controls/rollercoaster-controls/RollercoasterControls.js
+++ src/espinaco/controls/rollercoaster-controls/RollercoasterControls.js
@@ -1,14 +1,13 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import RollercoasterControlsClass from "./RollercoasterControlsClass";
 import { useThree } from "@react-three/fiber";
 
 
 export default function RollercoasterControls() {
   const { scene, camera } = useThree();
-  useEffect(() => {
-    const rollercoasterControls = new RollercoasterControlsClass({scene,camera});
-    console.log("rollercoasterControls class: ",rollercoasterControls);
-  }, []);
+  const rollercoasterControls = useMemo(()=> new RollercoasterControlsClass({scene,camera}));
+  console.log("rollercoasterControls class: ",rollercoasterControls);
+
 
   return null;
 }
