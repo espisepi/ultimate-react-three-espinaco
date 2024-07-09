9176c010e3fb9cc5d74742159d02553564a75694
espisepi <sepinaco@gmail.com>
2024-07-09 21:23:12 +0200

    update path my server media

--- src/espinaco/config/env.js
+++ src/espinaco/config/env.js
@@ -2,8 +2,8 @@
 export const env = {
 
     // PATH_MY_SERVER_MEDIA: "http://192.168.1.130:3000",
-    // PATH_MY_SERVER_MEDIA: "https://sepinaco.com:3000"
-    PATH_MY_SERVER_MEDIA: "https://188.26.206.195:3000",
+    PATH_MY_SERVER_MEDIA: "https://sepinaco.com:3000",
+    // PATH_MY_SERVER_MEDIA: "https://188.26.206.195:3000",
 
     // Esta url ya no existe
     BASE_URL_YT_DL_SERVER: "https://video-dl-esp.herokuapp.com/video/video?url="
