f6d1fcfc565d78bf6198ef6dcfdcb56ba1371dc5
espisepi <sepinaco@gmail.com>
2024-07-20 21:54:02 +0200

    improve App2

--- src/espinaco/apps/app2/App2.js
+++ src/espinaco/apps/app2/App2.js
@@ -1,9 +1,47 @@
+import CanvasDefault from "../../components/canvas/CanvasDefault";
+import CanvasXR from "../../components/canvas/CanvasXR";
+import ControlsManager from "../../controls/manager/ControlsManager";
+import SceneManager from "../../scenes/manager/SceneManager";
 
-
-export default function App2() {
-    return (
-        <>
-            <h1>Hola mundo app2</h1>
-        </>
-    )
+export default function App2({ xrmode = false, controlId = 1, sceneId = 0 }) {
+  return (
+    <>
+      <>
+      <h1>TODO: NO APARECE ROLLERCOASTER</h1>
+        {xrmode ? (
+          <CanvasXR
+            style={{
+              position: "relative",
+              top: "0",
+              width: "100%",
+              height: "100vh",
+            }}
+          >
+            <ControlsManager
+              id_control={controlId}
+              id_scene={sceneId}
+              xrmode={xrmode}
+            />
+            <SceneManager id={sceneId} />
+          </CanvasXR>
+        ) : (
+          <CanvasDefault
+            style={{
+              position: "relative",
+              top: "0",
+              width: "100%",
+              height: "100vh",
+            }}
+          >
+            <ControlsManager
+              id_control={controlId}
+              id_scene={sceneId}
+              xrmode={xrmode}
+            />
+            <SceneManager id={sceneId} />
+          </CanvasDefault>
+        )}
+      </>
+    </>
+  );
 }
