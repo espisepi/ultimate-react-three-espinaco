6f075ece7f7ff80a4a9ee3a0ee9aa843d737590c
espisepi <sepinaco@gmail.com>
2024-07-21 22:33:37 +0200

    frameRate low

--- src/espinaco/components/canvas/CanvasXR.js
+++ src/espinaco/components/canvas/CanvasXR.js
@@ -3,7 +3,7 @@ import { Canvas } from "@react-three/fiber";
 
 import { XR, createXRStore } from "@react-three/xr";
 
-const store = createXRStore({foveation: 1});
+const store = createXRStore({foveation: 1, frameRate: "low" });
 
 export default function CanvasXR({
   style = { position: "absolute", top: "0", width: "100%", height: "100vh" },
