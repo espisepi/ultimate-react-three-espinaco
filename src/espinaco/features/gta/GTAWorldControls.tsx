adbf383e92ba1ec6a9545e91f028395ba1672bf0
espisepi <sepinaco@gmail.com>
2024-07-26 18:06:59 +0200

    world helloworld

--- src/espinaco/features/gta/GTAWorldControls.tsx
+++ src/espinaco/features/gta/GTAWorldControls.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { World } from "./world/World";
 
 type GTAWorldControlsProps = {};
 
@@ -6,6 +7,7 @@ export const GTAWorldControls: React.FC<GTAWorldControlsProps> = React.memo(() =
   console.log("HELLO GTAWORLD");
   useEffect(() => {
     console.log("useeffect");
+    const gtaWorld = new World();
     return () => {
       console.log("useefect destruido");
     }
