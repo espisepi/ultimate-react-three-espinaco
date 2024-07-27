11763332582a45b9deebc17a4e7c9781d57d858e
espisepi <sepinaco@gmail.com>
2024-07-27 03:50:22 +0200

    plane no visible and format code

--- src/espinaco/features/car-vehicle-cannon/vehicles/raycastVehicle/RaycastVehicle.tsx
+++ src/espinaco/features/car-vehicle-cannon/vehicles/raycastVehicle/RaycastVehicle.tsx
@@ -1,56 +1,64 @@
 // This demo is also playable without installation here:
 // https://codesandbox.io/s/basic-demo-forked-ebr0x
 
-import type { CylinderArgs, CylinderProps, PlaneProps } from '@react-three/cannon'
-import { Debug, Physics, useCylinder, usePlane } from '@react-three/cannon'
-import { Environment, OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { Suspense, useRef } from 'react'
-import type { Group, Mesh } from 'three'
+import type {
+  CylinderArgs,
+  CylinderProps,
+  PlaneProps,
+} from "@react-three/cannon";
+import { Debug, Physics, useCylinder, usePlane } from "@react-three/cannon";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import type { Group, Mesh } from "three";
 
-import { useToggledControl } from './use-toggled-control'
-import Vehicle from './Vehicle'
+import { useToggledControl } from "./use-toggled-control";
+import Vehicle from "./Vehicle";
 
 function Plane(props: PlaneProps) {
-  const [ref] = usePlane(() => ({ material: 'ground', type: 'Static', ...props }), useRef<Group>(null))
+  const [ref] = usePlane(
+    () => ({ material: "ground", type: "Static", ...props }),
+    useRef<Group>(null)
+  );
   return (
-    <group ref={ref}>
-      <mesh receiveShadow>
+    <group ref={ref} visible={false}>
+      {/* <mesh receiveShadow> */}
+      <mesh>
         <planeGeometry args={[100, 100]} />
         <meshStandardMaterial color="#303030" />
       </mesh>
     </group>
-  )
+  );
 }
 
 function Pillar(props: CylinderProps) {
-  const args: CylinderArgs = [0.7, 0.7, 5, 16]
+  const args: CylinderArgs = [0.7, 0.7, 5, 16];
   const [ref] = useCylinder(
     () => ({
       args,
       mass: 10,
       ...props,
     }),
-    useRef<Mesh>(null),
-  )
+    useRef<Mesh>(null)
+  );
   return (
     <mesh ref={ref} castShadow>
       <cylinderGeometry args={args} />
       <meshNormalMaterial />
     </mesh>
-  )
+  );
 }
 
 const style = {
-  color: 'white',
-  fontSize: '1.2em',
+  color: "white",
+  fontSize: "1.2em",
   left: 50,
-  position: 'absolute',
+  position: "absolute",
   top: 20,
-} as const
+} as const;
 
 const VehicleScene = () => {
-  const ToggledDebug = useToggledControl(Debug, '?')
+  const ToggledDebug = useToggledControl(Debug, "?");
 
   return (
     <>
@@ -66,20 +74,27 @@ const VehicleScene = () => {
           penumbra={1}
           position={[10, 10, 10]}
         /> */}
-        <Physics
-          broadphase="SAP"
-          defaultContactMaterial={{ contactEquationRelaxation: 4, friction: 1e-3 }}
-          allowSleep
-        >
-          <ToggledDebug>
-            <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: 'floor' }} />
-            <Vehicle position={[0, 2, 0]} rotation={[0, -Math.PI / 4, 0]} angularVelocity={[0, 0.5, 0]} />
-            <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
+      <Physics
+        broadphase="SAP"
+        defaultContactMaterial={{
+          contactEquationRelaxation: 4,
+          friction: 1e-3,
+        }}
+        allowSleep
+      >
+        <ToggledDebug>
+          <Plane rotation={[-Math.PI / 2, 0, 0]} userData={{ id: "floor" }} />
+          <Vehicle
+            position={[0, 2, 0]}
+            rotation={[0, -Math.PI / 4, 0]}
+            angularVelocity={[0, 0.5, 0]}
+          />
+          {/* <Pillar position={[-5, 2.5, -5]} userData={{ id: 'pillar-1' }} />
             <Pillar position={[0, 2.5, -5]} userData={{ id: 'pillar-2' }} />
-            <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} />
-          </ToggledDebug>
-        </Physics>
-        {/* <Suspense fallback={null}>
+            <Pillar position={[5, 2.5, -5]} userData={{ id: 'pillar-3' }} /> */}
+        </ToggledDebug>
+      </Physics>
+      {/* <Suspense fallback={null}>
           <Environment preset="night" />
         </Suspense>
         <OrbitControls />
@@ -92,9 +107,9 @@ const VehicleScene = () => {
         </pre>
       </div> */}
     </>
-  )
-}
+  );
+};
 
 export const RaycastVehicle = () => {
-    return <VehicleScene />
-}
+  return <VehicleScene />;
+};
