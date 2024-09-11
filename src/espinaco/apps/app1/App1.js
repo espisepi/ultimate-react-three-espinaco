24f4fcb6082066520f0f7a29891ec9525e5a91a4
espisepi <sepinaco@gmail.com>
2024-09-11 02:24:38 +0200

    rename icon-container

--- src/espinaco/apps/app1/App1.js
+++ src/espinaco/apps/app1/App1.js
@@ -91,14 +91,16 @@ export function App1Start() {
         <CanvasManager />
 
         <div className="menu-container menu-1 active">
-          <button class="menu-button button-style icon-home">
+          <button className="menu-button button-style icon-container">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
               <path d="M280.37 148.26L96 300.11V464a16 16 0 0016 16l112-.29a16 16 0 0016-16V368a16 16 0 0116-16h64a16 16 0 0116 16v95.72a16 16 0 0016 16l112 .29a16 16 0 0016-16V300L295.67 148.26a12 12 0 00-15.3 0zM573.32 268.35L488 197.8V44a12 12 0 00-12-12h-72a12 12 0 00-12 12v72.61L318.47 43.19c-11.7-9.44-28.57-9.44-40.27 0L2.67 268.35a12 12 0 00-1.6 16.9l25.5 31.1a12 12 0 0016.9 1.6l235.4-190.53a12 12 0 0115.3 0l235.4 190.53a12 12 0 0016.9-1.6l25.5-31.1a12 12 0 00-1.65-16.9z"/>
             </svg>
             Abrir menu 1
             </button>
-          <button class="menu-button">Esconder botones</button>
-          <button class="menu-button">Abrir menu 2</button>
+          <button class="menu-button icon-container">Esconder botones</button>
+          <button class="menu-button icon-container">
+            Abrir menu 2
+          </button>
         </div>
 
         <div className="menu-container menu-2">
