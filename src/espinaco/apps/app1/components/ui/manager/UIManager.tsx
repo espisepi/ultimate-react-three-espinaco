d61e47f4abf91bcf771ec631965f54b3e5b077f4
espisepi <sepinaco@gmail.com>
2024-09-11 15:23:01 +0200

    improve UIMenu1

--- src/espinaco/apps/app1/components/ui/manager/UIManager.tsx
+++ src/espinaco/apps/app1/components/ui/manager/UIManager.tsx
@@ -4,9 +4,7 @@ import { UIMenu1 } from "../features/UIMenu1";
 export const UIManager = () => {
   return (
     <>
-      <div className="menu-container menu-1 active">
-        <UIMenu1 />
-      </div>
+      <UIMenu1 />
 
       <div className="menu-container menu-2">
         <h1>OYEEEEEEEEEE</h1>
