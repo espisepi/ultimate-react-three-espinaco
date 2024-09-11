aa59a6d7954fa6364ff5eb56b06ad14efc207718
espisepi <sepinaco@gmail.com>
2024-09-11 15:22:00 +0200

    Create UIManager to clean code

---
+++ src/espinaco/apps/app1/components/ui/manager/UIManager.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { UIMenu1 } from "../features/UIMenu1";
+
+export const UIManager = () => {
+  return (
+    <>
+      <div className="menu-container menu-1 active">
+        <UIMenu1 />
+      </div>
+
+      <div className="menu-container menu-2">
+        <h1>OYEEEEEEEEEE</h1>
+      </div>
+
+      <div className="menu-container menu-3">
+        <h1>OYEEEEEEEEEE</h1>
+      </div>
+    </>
+  );
+};
