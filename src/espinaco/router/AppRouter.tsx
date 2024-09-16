822e7167954580080f2c2c1b7099747dccf40ce6
espisepi <sepinaco@gmail.com>
2024-09-16 20:42:46 +0200

    recolocate by toc xD

--- src/espinaco/router/AppRouter.tsx
+++ src/espinaco/router/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App0 from "../apps/app0/App0";
+
 import { AppVideoPoints } from "../features/react-three-drei-espinaco/apps/videopoints-app/AppVideoPoints";
+import App0 from "../apps/app0/App0";
 import App2 from "../apps/app2/App2";
 
 export const AppRouter = () => {
