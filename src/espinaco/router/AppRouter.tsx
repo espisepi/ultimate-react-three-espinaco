be8711fb08d4a01bb49d351c22e48a17dfe16ea8
espisepi <sepinaco@gmail.com>
2024-09-16 18:38:38 +0200

    added react-router-dom

---
+++ src/espinaco/router/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App0 from "../app-module/apps/app0/App0";
+import App1 from "../app-module/apps/app1/App1";
+import App2 from "../app-module/apps/app2/App2";
+
+export const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App1 />} />
+        <Route path="/app0" element={<App0 />} />
+        <Route path="/app2" element={<App2 />} />
+      </Routes>
+    </Router>
+  );
+};
