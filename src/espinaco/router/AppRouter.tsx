8b38f9d7f1431f4013452909c928a6f72dfabe9e
espisepi <sepinaco@gmail.com>
2024-10-20 20:20:20 +0200

    remove unused code

--- src/espinaco/router/AppRouter.tsx
+++ src/espinaco/router/AppRouter.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { AppVideoPoints } from "../features/react-three-drei-espinaco/apps/videopoints/AppVideoPoints";
-import App0 from "../apps/app0/App0";
-import App2 from "../apps/app2/App2";
 import { AppBooks } from "../features/react-three-drei-espinaco/apps/books/AppBooks";
 
 export const AppRouter = () => {
@@ -12,8 +10,6 @@ export const AppRouter = () => {
         <Route path="/" element={<AppVideoPoints />} />
         <Route path="/books" element={<AppBooks />} />
         <Route path="/videopoints" element={<AppVideoPoints />} />
-        <Route path="/app0" element={<App0 />} />
-        <Route path="/app2" element={<App2 />} />
       </Routes>
     </Router>
   );
