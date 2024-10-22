16fb876bbdb811259fb9b9f5cfc37d4290617202
espisepi <sepinaco@gmail.com>
2024-10-22 13:40:34 +0200

    added react-transition-group

--- src/espinaco/features/react-three-drei-espinaco/apps/videopoints/features/ui/router/UIRouter.tsx
+++ src/espinaco/features/react-three-drei-espinaco/apps/videopoints/features/ui/router/UIRouter.tsx
@@ -1,16 +1,29 @@
 import { UIPage1 } from "../pages/page1/UIPage1";
 import { UIPage2 } from "../pages/page2/UIPage2";
 import { UIPage3 } from "../pages/page3/UIPage3";
-
+import { useUIStore } from "../store/UIStore";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 export const UIRouter = () => {
+  // Ver el estado de las pantallas
+  const { screen1, screen2, screen3 } = useUIStore((state) => state.screens);
+
+  console.log("EEE");
+
   return (
     <>
-      <UIPage1 />
+      {/* Aquí agregamos las transiciones */}
+      <TransitionGroup>
+        <CSSTransition timeout={300} classNames="fade">
+          <div>
+            {<UIPage1 />}
 
-      <UIPage2 />
+            {screen2 && <UIPage2 />}
 
-      <UIPage3 />
+            {screen3 && <UIPage3 />}
+          </div>
+        </CSSTransition>
+      </TransitionGroup>
     </>
   );
 };
