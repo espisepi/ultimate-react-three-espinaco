2fb4592ac11f0d103d802963b602c5bb56144149
espisepi <sepinaco@gmail.com>
2025-03-04 22:43:36 +0100

    AppGrid with products

--- src/espinaco/features/react-three-drei-espinaco/apps/grid/AppGrid.tsx
+++ src/espinaco/features/react-three-drei-espinaco/apps/grid/AppGrid.tsx
@@ -1,10 +1,35 @@
 import "./AppGrid.css";
 
+const products = [];
+
+for(let i = 0; i < 20; i++) {
+    products.push({
+        name: "product " + (i + 1)
+    });
+}
+
+const editorialBlock = {
+  name: "Editorial X",
+  showIn: 3,
+  size: 1
+};
+
+const editorialBlocks = [editorialBlock];
+
+
 export const AppGrid = () => {
   return (
     <>
       <div className="app-grid">
-        <h1>oyes</h1>
+        {
+            products.map( product => (
+                <>
+                <div className="product-card">
+                    <h1>{product.name}</h1>
+                </div>
+                </>
+            ))
+        }
       </div>
     </>
   );
